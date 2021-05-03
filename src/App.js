b3bbf0940ee1014ad722a233eb1704ef669fb111
KyleWalkerley <walkerleykyle@gmail.com>
2021-05-03 10:55:26 +0200

    Chapter8

--- src/App.js
+++ src/App.js
@@ -5,16 +5,63 @@ import Products from './Products.js';
 import JumboTronComponent from './JumboTronComponent.js';
 import UserForm from './UserForm.js';
 import GitHub from './GitHub.js'
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import GitHubUser from './GitHubUser';
+
 
 class App extends Component { 
   render() { 
     return (
       <div>
-        <GitHub /> 
+        <Header />
       </div>
     );
   }
 }
 
 export default App;
+
+class Header extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+        <div>            
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/github">GitHub</Nav.Link>                  
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Switch> 
+            <Route path="/github/user/:login/:id" component={GitHubUser} />                                                                
+            <Route path="/github" component={GitHub} />              
+            <Route exact path="/" component={Home} />             
+            <Route path="/*" component={NotFound} />                       
+          </Switch>   
+        </div>  
+      </BrowserRouter> 
+    )
+  }
+}
+
+class Home extends Component {
+  render(){
+    return (
+      <div>
+        Home 
+      </div>
+    )
+  }
+}
+
+class NotFound extends Component {
+  render(){
+    return <div>Not Found</div>
+  }
+}
--- src/GitHub.js
+++ src/GitHub.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import ReactLoading from 'react-loading';
-import { Media, Form, FormGroup, FormControl, Button } from 'react-bootstrap';
+import { Media, Form, FormGroup, FormControl, Button, Nav } from 'react-bootstrap';
 
 class GitHub extends Component { 
  
@@ -12,11 +12,14 @@ class GitHub extends Component {
             searchTerm:'',
             isLoading : true 
         };
+
+        this.handleChange = this.handleChange.bind(this);  
+        this.handleSubmit = this.handleSubmit.bind(this); 
  
     }
 
     componentDidMount(){
-        this.getGitHubData('greg');
+        //this.getGitHubData('greg');
     }
 
 
@@ -34,15 +37,10 @@ class GitHub extends Component {
     render() {
         const listUsers = this.state.data.map(user =>             
             <Media key={user.id}>
-                <a href={user.html_url}>    
-                    <img
-                        width={64}
-                        height={64}
-                        className="mr-3"
-                        src={user.avatar_url}
-                        alt="Generic placeholder"
-                    />
-                </a>
+                <Nav.Link to={`/github/user/${user.login}/${user.score}`}>
+                    <img width={64} height={64} src={user.avatar_url}
+                    alt="Image"/>
+                </Nav.Link>
                 <Media.Body>
                     <h5>Login: {user.login}</h5>
                     <p>Id: { user.id }</p>
@@ -74,5 +72,21 @@ class GitHub extends Component {
             </div>
         );
     } 
+
+
+    handleSubmit(e) {
+        e.preventDefault();    
+        this.setState({
+            isLoading : true          
+        })                
+        this.getGitHubData(this.state.searchTerm);        
+    }
+
+    handleChange(e) {
+        this.setState({ searchTerm: e.target.value });        
+    }
+
+
 }
+
 export default GitHub;
